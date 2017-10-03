6ecfc44cbb2feb48700c143406af8a0a30fbb461
Misha Wagner <mishajw@gmail.com>
2017-10-03 11:37:02 +0100

    Update main.py to use separate trainers

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from object_net import object_net_components
-from object_net import object_net_writer
-from object_net import padder
-from object_net import types
 import argparse
+import object_net_trainer
+import rnn_trainer
 import tensorflow as tf
 import tf_utils
 
@@ -10,6 +8,8 @@ import tf_utils
 def main():
     # Set up arguments
     parser = argparse.ArgumentParser()
+    parser.add_argument("--object_net", type=bool, default=False)
+    parser.add_argument("--rnn", type=bool, default=False)
     tf_utils.generic_runner.add_arguments(parser)
     tf_utils.data_holder.add_arguments(parser)
     args = parser.parse_args()
@@ -19,93 +19,17 @@ def main():
 
     # Set up data
     data = [[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]] * 100
-    data = map(sequence_type.get_state_output_pairs, data)
-    data = padder.PaddedData.from_unpadded(data)
-    data_placeholder = padder.PlaceholderPaddedData()
-    data_holder = tf_utils.data_holder.DataHolder(args, lambda i: data[i], len(data))
 
-    # Set up object_net model
-    def get_model(training: bool) -> object_net_writer.ObjectNetWriter:
-        return object_net_writer.ObjectNetWriter(
-            truth_padded_data=data_placeholder,
-            initial_hidden_vector_input=tf.zeros(args.batch_size),
-            object_type=sequence_type,
-            training=training,
-            hidden_vector_network=object_net_components.LstmHiddenVectorNetwork(
-                layer_size, num_layers, object_net_components.AdditionHiddenVectorCombiner()))
-    object_net_model = get_model(True)
-    object_net_model_test = get_model(False)
-    optimizer = tf.train.AdamOptimizer().minimize(object_net_model.cost)
-    tf.summary.scalar("object_net_cost", object_net_model.cost)
-
-    # Run training
-    def train_step(session, step, training_input, _, summary_writer, all_summaries):
-        result, cost, summaries = session.run(
-            [optimizer, object_net_model.cost, all_summaries],
-            data_placeholder.get_feed_dict(training_input))
-
-        summary_writer.add_summary(summaries, step)
-
-        print("Training error: %s" % cost)
-
-    def test_step(session, step, testing_input, _, summary_writer, all_summaries):
-        cost_result, all_summaries = session.run(
-            [object_net_model.cost, all_summaries],
-            data_placeholder.get_feed_dict(testing_input))
-
-        summary_writer.add_summary(all_summaries, step)
-
-        print("Test cost at step %d: %f" % (step, cost_result))
-
-        show_examples(session, testing_input)
-
-    def show_examples(session, model_input):
-        # Limit to 10 inputs
-        model_input = [x[:10] for x in model_input]
-
-        generated_states_padded, \
-            generated_outputs_padded, \
-            generated_outputs_counts_padded, \
-            generated_step_counts = session.run(
-                [
-                    object_net_model_test.generated_states_padded,
-                    object_net_model_test.generated_outputs_padded,
-                    object_net_model_test.generated_outputs_counts_padded,
-                    object_net_model_test.generated_step_counts],
-                data_placeholder.get_feed_dict(model_input))
-
-        copied_testing_input = padder.PaddedData(
-            generated_step_counts, generated_outputs_counts_padded, generated_states_padded, generated_outputs_padded)
-        unpadded = padder.unpad(copied_testing_input)
-
-        def try_array_to_value(_array):
-            try:
-                return list(sequence_type.get_value_from_state_output_pairs(_array))
-            except StopIteration:
-                return [-1]
-
-        generated_sequences = [try_array_to_value(array) for array in unpadded]
-
-        [print(s) for s in generated_sequences]
-
-    tf_utils.generic_runner.run_with_test_train_steps(
-        args,
-        "rnn_object_net_comparison",
-        get_batch_fn=lambda size: (data_holder.get_batch(size), None),
-        testing_data=(data_holder.get_test_data(), None),
-        test_step_fn=test_step,
-        train_step_fn=train_step)
-
-sequence_type = types.create_from_json("""
-{
-    "types": [
-        {
-            "base": "list",
-            "type": "float"
-        }
-    ]
-}
-""")[0]
+    if args.object_net and args.rnn:
+        print("Only one of object_net and rnn must be specified")
+    elif args.object_net:
+        with tf.variable_scope("object_net"):
+            object_net_trainer.train(args, data, layer_size, num_layers)
+    elif args.rnn:
+        with tf.variable_scope("rnn"):
+            rnn_trainer.train(args, data, layer_size, num_layers)
+    else:
+        print("One of object_net and rnn must be specified")
 
 
 if __name__ == "__main__":
